1f70261791e6c399429871adc3b5b01e50c3dce5
lybenson <naivebenson@gmail.com>
2018-03-27 01:58:33 +0800

    fill other operation

--- node_command/README.md
+++ node_command/README.md
@@ -18,3 +18,95 @@ node.js命令行程序主要依赖于commander.js
 
 
 脚本示例可查看command.js文件
+
+`node-cmd`提供了对shell脚本的支持, 执行`npm install node-cmd`即可安装
+
+```
+var cmd=require('node-cmd');
+
+cmd.get(
+  'pwd',
+  function(err, data, stderr){
+      console.log('the current working dir is : ', data)
+  }
+);
+cmd.run('touch example.created.file');
+  cmd.get(
+    `
+      git clone https://github.com/RIAEvangelist/node-cmd.git
+      cd node-cmd
+      ls
+    `,
+    function(err, data, stderr){
+      if (!err) {
+          console.log('the node-cmd cloned dir contains these files :\n\n',data)
+      } else {
+          console.log('error', err)
+      }
+    }
+  );
+```
+
+
+`chalk`是一个日志的样式库，可以在终端上面调整日志的样式，支持文字颜色和背景颜色
+
+```
+const chalk = require('chalk');
+
+console.log(chalk.red('hello world'));
+```
+
+更多的颜色可以通过其github查看
+
+
+`ora`是一个终端实现Loading的效果
+```
+const ora = require('ora');
+
+const spinner = ora('Loading unicorns').start();
+
+setTimeout(() => {
+	spinner.color = 'yellow';
+	spinner.text = 'Loading rainbows';
+}, 1000);
+```
+
+实例方法
+
+- start([text])
+- stop()
+- succeed([text])
+- fail([text])
+- warn([text])
+- info([text])
+- stopAndPersist([options])
+- clear()
+- render()
+- frame()
+
+实例属性
+
+- text: 改变字体
+- color: 改变Loading颜色
+
+
+`inquirer` 实现终端与用户交互, 如选择文件类型等
+
+```
+var inquirer = require('inquirer');
+inquirer.prompt([{type: 'confirm', name: 'OK', message: 'Are you OK?', default: false}]).then(answers => {
+    console.log(answers);
+});
+```
+
+type 取值有
+
+- list: 选择列表中一项
+- rawlist: 选择列表中一项
+- expand
+- checkbox: 多选
+- confirm: yes or no
+- input: 手动输入, 可正则校验
+- password: 输入密码, 星号
+- editor
+
--- node_command/command.js
+++ node_command/command.js
@@ -1,19 +1,67 @@
 #! /usr/bin/env node
 var commander = require('commander');
- 
+var cmd = require('node-cmd')
+var chalk = require('chalk');
+var ora = require('ora');
+var inquirer = require('inquirer');
+
+
+// option 表示可选的命令, 第一个参数是可选命令, 第二个参数是命令解释, 同时还可以设置读取用户输入的参数
+// parse(process.argv); // 将控制台得到的参数进行解析，调用相应的事件
 commander
   .version('0.0.1')
-  .option('-p, --ppp', 'p命令的解释')
-  .option('-b, --bbb', 'b命令的解释')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .option('-p, --prod', 'p命令的解释')
+  .option('-b, --build', 'b命令的解释')
+  .option('-t, --test [filename]', 't命令解释')
   .parse(process.argv);
- 
 
-console.log(commander)
-
-if (commander.p) {
-  console.log('23333')
+if (commander.ppp) { // 监听--ppp或-p
+  console.log('ppp命令接收到')
 }
+// 测试: proc -p
+// 输出: ppp命令接收到
 
-// option 表示可选的命令, 第一个参数是可选命令, 第二个参数是命令解释
-// parse(process.argv); // 将控制台得到的参数进行解析，调用相应的事件
+// command 在命令行增加一个命令。用户在执行此命令后，能够执行回调中的逻辑
+// option 表示可选参数
+// alias 命令的简写形式
+// description 命令描述
+// action 输入命令后执行方法
+commander
+  .command('init <pname> [oname...]')
+  .option('-w, --watch', '监听文件变化')
+  .alias('i')
+  .description('init project')
+  .action(function (pname, oname, command) {
+    console.log(arguments.length)
+    console.log(`init ${chalk.magenta(pname)} success`)
+    console.log(oname)
+    console.log(command.watch)
+
+    setTimeout(() => {
+      spinner.color = 'yellow';
+      spinner.text = 'Loading rainbows';
+      setTimeout(() => {
+        spinner.succeed('loading success')
+      }, 2000);
+    }, 1000);
+
+    inquirer.prompt([{
+      type: 'list',
+      name: 'size',
+      message: 'What size do you need?',
+      choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+      filter: function(val) {
+        return val.toLowerCase();
+        }
+      }]).then(answers => {
+        console.log(JSON.stringify(answers, null, '  '));
+      });
+  })
+commander.parse(process.argv);
+
+const spinner = ora('Loading unicorns').start();
+
+
+
+// 测试: proc i pname_t oname_t -w
+// 输出: 3  pname_t  [ 'oname_t' ]  true
